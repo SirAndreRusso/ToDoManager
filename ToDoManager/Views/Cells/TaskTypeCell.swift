c96d60035ff0c4c7e55a80452093c74ca0ac6ce5
SirAndreRusso <102429266+SirAndreRusso@users.noreply.github.com>
2022-06-22 16:54:07 +0300

    В приложение добавлено 2 новых экрана для редактирования и создания новых задач

---
+++ ToDoManager/Views/Cells/TaskTypeCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskTypeCell.swift
+//  ToDoManager
+//
+//  Created by Андрей Русин on 21.06.2022.
+//
+
+import UIKit
+
+class TaskTypeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
